0a1b4bfc75f382d366c6c6661ae7eb8757a4d5d1
Tiancheng Mao <solleo1989@gmail.com>
2025-05-15 15:09:13 -0700

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import { ColorModeButton } from "./components/ui/color-mode";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
@@ -10,8 +12,8 @@ const App = () => {
           lg: `"nav nav" "aside main"`, // > 1024px
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <NavBar></NavBar>
         </GridItem>
         <Show when="lg">
           <GridItem
@@ -26,7 +28,7 @@ const App = () => {
           Main
         </GridItem>
       </Grid>
-      <Button colorScheme="red">Button</Button>
+      <ColorModeButton colorScheme="red">Button</ColorModeButton>
       <Button colorPalette="red">Button</Button>
       <Button colorPalette="blue" variant="outline">
         Button
